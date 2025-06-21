c7acb0c84bd98503b4d4bd3c30b8d93522621027
abdul ansari <samunleash1@yahoo.com>
2025-06-21 06:26:36 +0400

    dummy

--- sh/cd_pipeline.sh
+++ sh/cd_pipeline.sh
@@ -7,20 +7,16 @@ LOG_FILE="$SCRIPT_DIR/erp_deploy.log"
   echo "=============================="
   echo "Deploy cdapp script started at $(date)"
 
-  cd ~
-
-  source .zshrc
-
   cd /home/wethinkdigital/myapps/cd_pipeline || { echo "Failed to change directory"; exit 1; }
 
   echo "Pulling latest code from Git..."
   git pull || { echo "Git pull failed"; exit 1; }
 
   echo "download dependencies"
-  go mod download || { echo "building the go app failed"; exit 1; }
+  /usr/local/bin/go mod download || { echo "building the go app failed"; exit 1; }
 
   echo "building the go app"
-  go build -o app || { echo "building the go app failed"; exit 1; }
+  /usr/local/bin/go build -o app || { echo "building the go app failed"; exit 1; }
 
   echo "restarting the app"
   systemctl restart cdapp.service || { echo "restarting the app failed"; exit 1; }
