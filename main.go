7b892bc55cafd709673d47494a99765d829ba9cd
abdul ansari <samunleash1@yahoo.com>
2025-06-21 04:38:36 +0400

    added crm sh

--- main.go
+++ main.go
@@ -39,9 +39,26 @@ func cdHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Deploy triggered %v", w)
 }
 
+func crmHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		return
+	}
+	// You can parse the payload here if needed
+	go func() {
+		cmd := exec.Command("./sh/crm.sh")
+		err := cmd.Run()
+		if err != nil {
+			log.Printf("Deploy failed: %v", err)
+		}
+	}()
+	log.Printf("Deploy triggered %v", w)
+}
+
 func main() {
 	http.HandleFunc("/erp_backend", handler)
 	http.HandleFunc("/cd", cdHandler)
+	http.HandleFunc("/crm", crmHandler)
 	fmt.Println("Listening on :9000")
 	log.Fatal(http.ListenAndServe(":9000", nil))
 }
