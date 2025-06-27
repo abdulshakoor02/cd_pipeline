c6929f149d39e13be2862c8e8bc95a11e68e7f44
abdul ansari <samunleash1@yahoo.com>
2025-06-27 23:39:41 +0400

    fixed sh

--- sh/crm.sh
+++ sh/crm.sh
@@ -3,6 +3,10 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 LOG_FILE="$SCRIPT_DIR/erp_deploy.log"
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 {
   echo "=============================="
   echo "Deploy crm script started at $(date)"
